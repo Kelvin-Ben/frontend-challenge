65019320772bb5485546a4975df44493a93b3f2e
Kelvin-Ben <kevoben14@gmail.com>
2024-10-31 03:48:12 +0300

    Implemented TodoFilter component to display filter options and clear completed todos

--- src/components/TodoFilter.tsx
+++ src/components/TodoFilter.tsx
@@ -1,37 +1,36 @@
-interface Todo {
-  id: number;
-  title: string;
-  completed: boolean;
-}
-interface TodoFilterProps {
-  todos: Todo[];
-  onClearCompleted: () => void;
+import useTodoActions from "../hooks/useTodoActions";
+interface TodoFilterProp {
   setFilter: (filter: "all" | "active" | "completed") => void;
+  filter: "all" | "active" | "completed";
 }
-const TodoFilter: React.FC<TodoFilterProps> = ({
-  todos,
-  onClearCompleted,
-  setFilter,
-}) => {
+const TodoFilter = ({ setFilter, filter }: TodoFilterProp) => {
+  const { todos, clearCompleted } = useTodoActions();
   const activeCount = todos.filter((todo) => !todo.completed).length;
+
   return (
     <div className="flex flex-col sm:flex-row justify-between items-center mt-8 px-0 gap-4">
       <div className="text-gray-600 font-bold">{activeCount} items left</div>
       <div className="flex flex-wrap justify-center space-x-4 sm:space-x-2 gap-2 sm:gap-4">
         <button
-          className="bg-transparent p-0 text-gray-500 hover:underline font-semibold"
+          className={`bg-transparent p-0 hover:underline font-semibold ${
+            filter === "all" ? "text-blue-900" : "text-gray-500"
+          }`}
           onClick={() => setFilter("all")}
         >
           All
         </button>
         <button
-          className="bg-transparent p-0 text-gray-500 hover:underline font-semibold"
+          className={`bg-transparent p-0 hover:underline font-semibold ${
+            filter === "active" ? "text-blue-900" : "text-gray-500"
+          }`}
           onClick={() => setFilter("active")}
         >
           Active
         </button>
         <button
-          className="bg-transparent p-0 text-gray-500 hover:underline font-semibold"
+          className={`bg-transparent p-0 hover:underline font-semibold ${
+            filter === "completed" ? "text-blue-900" : "text-gray-500"
+          }`}
           onClick={() => setFilter("completed")}
         >
           Completed
@@ -39,7 +38,7 @@ const TodoFilter: React.FC<TodoFilterProps> = ({
       </div>
       <button
         className="hover:text-red-400 bg-transparent p-0 text-gray-500 font-semibold"
-        onClick={onClearCompleted}
+        onClick={clearCompleted}
       >
         Clear Completed
       </button>
