9b6ed2ec80d19ac77a33d446df16727efafdcb2a
Kelvin-Ben <kevoben14@gmail.com>
2024-10-31 03:43:43 +0300

    Created ThemeContext and ThemeProvider components to manage dark mode theme state and provide context for components

--- src/context/ThemeProvider.tsx
+++ src/context/ThemeProvider.tsx
@@ -3,27 +3,27 @@ import useTheme from "../hooks/useTheme";
 
 interface ThemeContextProps {
   darkMode: boolean;
-  toggleDarkMode: () => void
+  toggleDarkMode: () => void;
 }
 const ThemeContext = createContext<ThemeContextProps | undefined>(undefined);
 
 interface ThemeProviderProps {
-  children: React.ReactNode
+  children: React.ReactNode;
 }
 
-export const ThemeProvider: React.FC<ThemeProviderProps> = ({children}) => {
-  const { darkMode, toggleDarkMode} = useTheme()
+export const ThemeProvider: React.FC<ThemeProviderProps> = ({ children }) => {
+  const { darkMode, toggleDarkMode } = useTheme();
   return (
-    <ThemeContext.Provider value={{darkMode, toggleDarkMode}}>
+    <ThemeContext.Provider value={{ darkMode, toggleDarkMode }}>
       {children}
     </ThemeContext.Provider>
-  )
-}
+  );
+};
 
 export const useThemeContext = (): ThemeContextProps => {
   const context = useContext(ThemeContext);
   if (!context) {
-    throw new Error('useThemeContext must be used within a ThemeProvider');
+    throw new Error("useThemeContext must be used within a ThemeProvider");
   }
-  return context
-}
+  return context;
+};
